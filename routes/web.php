6693db1d47f52903c5571b7ae473d93c572d896e
ervinosmani3@gmail.com <ervinosmani3@gmail.com>
2025-04-17 01:41:31 +0200

    Add temporary /run-migrations route for production DB setup on Render

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Rruga per migrate - PERDOR VETEM NJE HERE dhe fshije pas perdorimit!
+Route::get('/run-migrations', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Migrimet u ekzekutuan me sukses ✅';
 });
